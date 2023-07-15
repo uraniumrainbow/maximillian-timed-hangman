e31a6d2a69cc756b91299cb487adb74f2f923daa
uraniumrainbow <uraniumrainbow@gmail.com>
2023-07-15 13:41:10 -0600

    quiz quesitons formatted

---
+++ assets/script.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options:
+        [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+    numb: 2,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+        "eXtensible Markup Language",
+        "eXecutable Multiple Language",
+        "eXtra Multiprogram Language",
+        "eXamine Multiple Language"
+        ]
+    },
+    {
+    numb: 3,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+        "Stylish Question Language",
+        "Stylesheet Query Language",
+        "Statement Question Language",
+        "Structured Query Language"
+        ]
+    },
+    {
+    numb: 4,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheets",
+    options: [
+        "Cascada Style Script",
+        "Cascading Style Sheets",
+        "Casted Script Style",
+        "Calculated Scripting Science"
+        ]
+    },
+    {
+    numb: 5,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+        "Hypertext Programming",
+        "Hypertext Postprocessing",
+        "Homosapiens Philosophy",
+        "Hypertext Preprocessor"
+        ]
+    },
+
+]
